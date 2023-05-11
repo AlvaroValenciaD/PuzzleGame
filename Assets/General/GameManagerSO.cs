16471329a477714ed583e440e317217c1508651c
AlvaroValenciaD <alvarovalenciadegracia@gmail.com>
2023-05-11 17:31:18 +0200

    Solucion 1

--- Assets/General/GameManagerSO.cs
+++ Assets/General/GameManagerSO.cs
@@ -8,29 +8,34 @@ public class GameManagerSO : ScriptableObject
 {
     [SerializeField] private int indexPuertaObjetivo = -1;
 
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+    //private void OnEnable()
+    //{
+    //    SceneManager.sceneLoaded += SceneManager_sceneLoaded;
         
-    }
+    //}
 
-    private void SceneManager_sceneLoaded(Scene escenaCargada, LoadSceneMode sceneMode)
+    //private void SceneManager_sceneLoaded(Scene escenaCargada, LoadSceneMode sceneMode)
+    //{
+    //    Debug.Log("Nueva escena!");
+    //    if (indexPuertaObjetivo == -1) return;
+
+    //    Door[] doors = FindObjectsOfType<Door>();
+    //    GameObject player = GameObject.FindWithTag("Player");
+    //    foreach (Door door in doors)
+    //    {
+    //        if(door.GetIndexDoor() == indexPuertaObjetivo)
+    //        {
+    //            Debug.Log("Vete a la puerta " + door.GetIndexDoor() + " de la escena " + escenaCargada.name);
+    //            player.transform.position = door.GetSpawnPoint().position;
+    //            player.transform.eulerAngles = door.GetSpawnRotation();
+    //            break;
+    //        }
+    //    }
+    //}
+
+    public int GetIndexPuertaObjetivo()
     {
-        Debug.Log("Nueva escena!");
-        if (indexPuertaObjetivo == -1) return;
-
-        Door[] doors = FindObjectsOfType<Door>();
-        GameObject player = GameObject.FindWithTag("Player");
-        foreach (Door door in doors)
-        {
-            if(door.GetIndexDoor() == indexPuertaObjetivo)
-            {
-                Debug.Log("Vete a la puerta " + door.GetIndexDoor() + " de la escena " + escenaCargada.name);
-                player.transform.position = door.GetSpawnPoint().position;
-                player.transform.eulerAngles = door.GetSpawnRotation();
-                break;
-            }
-        }
+        return indexPuertaObjetivo;
     }
     public void CargarEscena(int indexEscena, int indexPuerta)
     {
--- Assets/Player/Scripts/PlayerREJA.cs
+++ Assets/Player/Scripts/PlayerREJA.cs
@@ -5,14 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class PlayerREJA: MonoBehaviour
 {
+    [SerializeField] GameManagerSO gM;
     [Header("Movimiento")] 
     CharacterController chController;
     [SerializeField] float moveSpeed;[SerializeField] float gravityScale;
     Vector3 moveDirection; Vector3 movementY;
     float h, v;
+
+    bool shouldSpawn = false;
+    Vector3 spawnPosition;
     //[Header("Puertas")]
     //[SerializeField] LayerMask whatIsDoor;[SerializeField] float radiusDoor;[SerializeField] GameObject doorPoint;
 
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+
+    }
 
     void Start()
     {
@@ -23,20 +32,55 @@ public class PlayerREJA: MonoBehaviour
 
     void Update()
     {
-        h = Input.GetAxisRaw("Horizontal");
-        v = Input.GetAxisRaw("Vertical");
+        if (!shouldSpawn)
+        {
+            h = Input.GetAxisRaw("Horizontal");
+            v = Input.GetAxisRaw("Vertical");
 
-        // Multiplicamos *(-45) gardos para que tenga un movimiento coherente con el input de teclas
-        moveDirection = Quaternion.Euler(0, -45, 0) * new Vector3(h, 0, v);
-        chController.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
-        Gravity();
+            // Multiplicamos *(-45) gardos para que tenga un movimiento coherente con el input de teclas
+            moveDirection = Quaternion.Euler(0, -45, 0) * new Vector3(h, 0, v);
+        
+            chController.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
+            Gravity();
+        }
 
     }
 
+    private void LateUpdate()
+    {
+        if(shouldSpawn)
+        {
+            Debug.Log("Me recolocan");
+            transform.position = spawnPosition;
+            shouldSpawn = false;
+        }
+    }
     void Gravity()
     {
         movementY.y += gravityScale * Time.deltaTime;
         chController.Move(movementY * Time.deltaTime);
+    }
+
+
+    private void SceneManager_sceneLoaded(Scene escenaCargada, LoadSceneMode sceneMode)
+    {
+        Debug.Log("Nueva escena!");
+        if (gM.GetIndexPuertaObjetivo() == -1) return;
+
+        Door[] doors = FindObjectsOfType<Door>();
+        foreach (Door door in doors)
+        {
+            if (door.GetIndexDoor() == gM.GetIndexPuertaObjetivo())
+            {
+                //Debug.Log("Vete a la puerta " + door.GetIndexDoor() + " de la escena " + escenaCargada.name);
+                //transform.position = door.GetSpawnPoint().position;
+                spawnPosition = door.GetSpawnPoint().position;
+                //transform.eulerAngles = door.GetSpawnRotation();
+                
+                shouldSpawn = true;
+                break;
+            }
+        }
     }
 
     private void OnDrawGizmos()
