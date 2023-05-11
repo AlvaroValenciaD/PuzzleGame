c31ed4737def1542658f743f4c80e58ccbca479f
AlvaroValenciaD <alvarovalenciadegracia@gmail.com>
2023-05-11 17:49:05 +0200

    Bug arreglado

--- Assets/General/GameManagerSO.cs
+++ Assets/General/GameManagerSO.cs
@@ -8,34 +8,32 @@ public class GameManagerSO : ScriptableObject
 {
     [SerializeField] private int indexPuertaObjetivo = -1;
 
-    //private void OnEnable()
-    //{
-    //    SceneManager.sceneLoaded += SceneManager_sceneLoaded;
-        
-    //}
-
-    //private void SceneManager_sceneLoaded(Scene escenaCargada, LoadSceneMode sceneMode)
-    //{
-    //    Debug.Log("Nueva escena!");
-    //    if (indexPuertaObjetivo == -1) return;
-
-    //    Door[] doors = FindObjectsOfType<Door>();
-    //    GameObject player = GameObject.FindWithTag("Player");
-    //    foreach (Door door in doors)
-    //    {
-    //        if(door.GetIndexDoor() == indexPuertaObjetivo)
-    //        {
-    //            Debug.Log("Vete a la puerta " + door.GetIndexDoor() + " de la escena " + escenaCargada.name);
-    //            player.transform.position = door.GetSpawnPoint().position;
-    //            player.transform.eulerAngles = door.GetSpawnRotation();
-    //            break;
-    //        }
-    //    }
-    //}
-
-    public int GetIndexPuertaObjetivo()
+    private void OnEnable()
     {
-        return indexPuertaObjetivo;
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+        
+    }
+
+    private void SceneManager_sceneLoaded(Scene escenaCargada, LoadSceneMode sceneMode)
+    {
+        Debug.Log("Nueva escena!");
+        if (indexPuertaObjetivo == -1) return;
+
+        Door[] doors = FindObjectsOfType<Door>();
+        PlayerREJA player = FindObjectOfType<PlayerREJA>();
+        foreach (Door door in doors)
+        {
+            if(door.GetIndexDoor() == indexPuertaObjetivo)
+            {
+                Debug.Log("Vete a la puerta " + door.GetIndexDoor() + " de la escena " + escenaCargada.name);
+                player.GetComponent<CharacterController>().enabled = false;
+                Vector3 spawnPosition = door.GetSpawnPoint().position;
+                player.transform.position = spawnPosition;
+                player.transform.eulerAngles = door.GetSpawnRotation();
+                player.GetComponent<CharacterController>().enabled = true;
+                break;
+            }
+        }
     }
     public void CargarEscena(int indexEscena, int indexPuerta)
     {
