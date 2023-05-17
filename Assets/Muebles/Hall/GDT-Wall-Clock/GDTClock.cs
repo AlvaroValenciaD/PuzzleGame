f1e04581c2da4d477766ba33b0c18bc378597bb7
esdelrocio <esteladelrocio.carmona_4968@escuelaces.net>
2023-05-17 20:52:47 +0200

    Deco_Props
    Decoración de props e integración de texturas.

---
+++ Assets/Muebles/Hall/GDT-Wall-Clock/GDTClock.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GDTClock : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject hourHand;
+    [SerializeField]
+    private GameObject minuteHand;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetTime(int hour, int minute)
+    {
+
+        if(hour>=0 && hour < 24)
+        {
+            if (hour > 12)
+            {
+                hour -= 12;
+            }
+            hourHand.transform.localEulerAngles = new Vector3(0,0,(hour+minute/60f)*360f/12f);
+
+        }
+
+        if(minute>=0&& minute < 60)
+        {
+            minuteHand.transform.localEulerAngles = new Vector3(0, 0, minute * 360f / 60f);
+        }
+
+    }
+
+
+}
---
+++ Assets/Muebles/Hall/GDT-Wall-Clock/WallClockTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallClockTest : MonoBehaviour
+{
+
+    private GDTClock clock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clock = FindObjectOfType<GDTClock>();
+        InvokeRepeating("SetRandomTime",0,5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetRandomTime()
+    {
+        int hour = Random.Range(0,23);
+        int minute = Random.Range(0, 59);
+        Debug.Log("Current Time: " + hour + ":" + minute);
+        clock.SetTime(hour, minute);
+        
+    }
+
+}
