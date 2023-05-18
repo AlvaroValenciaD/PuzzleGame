6f941a1728c8f22e6e9fa8513d1c78d7a60f0b72
Ruth250 <ruthgarper@gmail.com>
2023-05-18 17:59:12 +0200

    Start_Inventory
    Comienzo del inventario

---
+++ Assets/Scenes/EscenaEjemploInventario/InventoryManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+    public List<Item> Items = new List<Item>();
+
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Add(Item item)
+    {
+        Items.Add(item);
+    }
+
+    public void ListItem()
+    {
+        foreach (var item in Items)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            //var itemName = obj.transform.Find("Item/ItemName").GetComponent<Text>();
+        }
+    }
+}
